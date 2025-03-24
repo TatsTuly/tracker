62dfbf4365f45a08174b6f77a2e6ce47f04214a9
TatsTuly <tuly15-4902@diu.edu.bd>
2025-03-24 22:35:27 +0600

    01

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("The Tracker Sheet"),
+          backgroundColor: const Color.fromARGB(255, 11, 157, 197),
+        ),
+        body: Center(
+          child: Text(
+            "Hello World!",
+            style: TextStyle(
+              fontFamily: "Raleway",
+              fontSize: 40,
+              color: const Color.fromARGB(255, 98, 9, 67),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
