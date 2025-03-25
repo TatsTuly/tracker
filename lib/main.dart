31b59cec6c32db217bb888ee90c63cca29faa8ee
TatsTuly <tuly15-4902@diu.edu.bd>
2025-03-25 13:28:52 +0600

    fixed_HelloWorld

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,33 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _displayText = "Don't Press the button";
-  void _changeText(){
-    setState(() {
-      _displayText = "Button pressed";
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                _displayText,
-                style: TextStyle(fontSize: 30),
-              ),
-              ElevatedButton(onPressed: _changeText, child: Text("Press")),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+ 
+ void main() {
+   runApp(const MyApp());
+ }
+ 
+ class MyApp extends StatelessWidget {
+   const MyApp({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       home: Scaffold(
+         appBar: AppBar(
+           centerTitle: true,
+           title: Text("The Tracker Sheet"),
+           backgroundColor: const Color.fromARGB(255, 11, 157, 197),
+         ),
+         body: Center(
+           child: Text(
+             "Hello World!",
+             style: TextStyle(
+               fontFamily: "Raleway",
+               fontSize: 40,
+               color: const Color.fromARGB(255, 98, 9, 67),
+             ),
+           ),
+         ),
+       ),
+     );
+   }
+ }
