e8e0703b114ec8464b2a472be9b253195c2727d9
TatsTuly <tuly15-4902@diu.edu.bd>
2025-03-26 22:11:47 +0600

    problem5

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,67 @@
 import 'package:flutter/material.dart';
- 
- void main() {
-   runApp(const MyApp());
- }
- 
- class MyApp extends StatelessWidget {
-   const MyApp({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: Scaffold(
-         appBar: AppBar(
-           centerTitle: true,
-           title: Text("The Tracker Sheet"),
-           backgroundColor: const Color.fromARGB(255, 11, 157, 197),
-         ),
-         body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Center(
-                child: Text("Item Number$index",selectionColor: Color.fromARGB(0, 16, 164, 36),),
-              
-              ),
-            );
-          },
-        ),
-       ),
-     );
-   }
- }
+
+void main() {
+  runApp(const NavigationApp());
+}
+
+class NavigationApp extends StatelessWidget {
+  const NavigationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('asset/images/catpic1.jpeg'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondScreen()),
+              );
+            },
+            child: const Text('Meet My MEW MEW'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('asset/images/download.jpeg'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Back to First MEW MEW'),
+          ),
+        ],
+      ),
+    );
+  }
+}
