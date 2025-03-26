60130b0000351b23d3c846affd7168d03518296c
TatsTuly <tuly15-4902@diu.edu.bd>
2025-03-26 22:19:13 +0600

    04

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const NavigationApp());
+  runApp(const StyledTextApp());
 }
 
-class NavigationApp extends StatelessWidget {
-  const NavigationApp({super.key});
+class StyledTextApp extends StatelessWidget {
+  const StyledTextApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const FirstScreen(),
+      home: const TextStyleScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
+class TextStyleScreen extends StatelessWidget {
+  const TextStyleScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('asset/images/catpic1.jpeg'),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SecondScreen()),
-              );
-            },
-            child: const Text('Meet My MEW MEW'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('asset/images/download.jpeg'),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Back to First MEW MEW'),
-          ),
-        ],
+      body: Center( 
+        child: Column(
+          mainAxisSize: MainAxisSize.min, 
+          children: [
+            const Text(
+              'Oi Kire! Kire?!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 12, 5, 110),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Modhu Modhuuu',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                color: Color.fromARGB(255, 165, 12, 6),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Roshmalaiiii',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                decoration: TextDecoration.underline,
+                color: Color.fromARGB(255, 45, 193, 39),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Kaka Hena Kothai? Henaaa',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: Color.fromARGB(255, 210, 20, 150),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
